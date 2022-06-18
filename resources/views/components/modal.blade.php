9015ee0e108d4ab00e2375330fa64136888edf94
natainditama <natainditama@gmail.com>
2022-06-18 13:31:42 +0800

    refactor: Add modal box blade components

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,7 @@
+<div id="wardrobeModal" class="modal-box p-4 overflow-hidden fixed z-50 w-full inset-0 h-full bg-black/50">
+    <div class="relative w-full max-w-2xl h-full md:h-auto mx-auto top-1/2 -translate-y-1/2 overflow-auto rounded-lg grid place-items-center">
+        <div class="modal-content relative bg-white rounded-lg shadow mx-auto w-full">
+            @yield('wardobeModal')
+        </div>
+    </div>
+</div>
