ccf2c1f385d6a090dd0fbb5d3ac834c9bb77cd92
natainditama <natainditama@gmail.com>
2022-06-18 13:09:16 +0800

    feat/hot reload: Install and Setup a browser-sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,9 +10,6 @@ const mix = require("laravel-mix");
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [require("tailwindcss")]
-);
+mix.browserSync("localhost:8000");
+mix.js("resources/js/app.js", "public/js");
+mix.postCss("resources/css/app.css", "public/css", [require("tailwindcss")]);
