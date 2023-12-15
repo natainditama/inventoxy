dadc5f3b63e54f31a8bd9882533608d1a449c464
Nata Inditama <natainditama.dev@gmail.com>
2023-12-15 19:04:04 +0800

    style: adjust header alignment

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,10 @@
 <header class="bg-white border-gray-200 py-6 rounded fixed inset-x-0 min-h-20 z-40">
-    <div class="container flex flex-wrap justify-between items-center mx-auto px-4">
+    <div @class([ "container flex flex-wrap justify-center items-center mx-auto px-4" , "!justify-between"=> Auth::check(),])>
         <a href="/" class="flex items-center">
             <span class="text-3xl font-bold">Inventoxy</span>
             <span class="h-1.5 aspect-square bg-red-500 rounded-full self-end mb-1 -ml-0.5"></span>
         </a>
+        @if (Auth::check())
         <button id="mobile-button" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="mobile-menu" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -13,7 +14,6 @@
                 <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
             </svg>
         </button>
-        @if (Auth::check())
         @include('components.navbar')
         @endif
     </div>
